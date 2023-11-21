de259c7bacd96b26f7c52e501fa08967d0ca646f
HcodNik <bruchikhin@gmail.com>
2023-11-21 16:57:03 +0300

    DZ # 5

---
+++ main_menu.py
@@ -0,0 +1,36 @@
+import os
+
+
+# ************ФУНКЦИИ************
+def sim_multi(sim, value): # функция символы
+    sim_multi = sim * value
+    return print(sim_multi)
+
+def add_folder(): # создать каталог
+    name_fold = input('Введите имя каталога: ')
+    while os.path.isdir(name_fold) == True:
+        print('Каталог с таким именем уже существует!')
+        return add_folder()
+    else:
+        print(f'Папка "{name_fold}" успешно создана.')
+        main_menu_explorer()
+        return os.mkdir(name_fold)
+
+def main_menu_explorer():
+    print("\033[3m\033[33m\033[41m{}\033[0m".format("Файловый менеджер вер. 1.0"))
+    sim_multi('#', 50)
+    print('1. Создать папку')
+    print('2. Удалить (файл/папку)')
+    print('3. Копировать (файл/папку)')
+    print('4. Просмотр содержимого рабочей директории')
+    print('5. Посмотреть только папки')
+    print('6. Посмотреть только файлы')
+    print('7. Просмотр информации об операционной системе')
+    print('8. Создатель программы')
+    print('9. Играть в викторину')
+    print('10. Мой банковский счет')
+    print('11. Смена рабочей директории')
+    print('12. Выход')
+    sim_multi('#', 50)
+
+add_folder()
