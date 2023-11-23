1675bed30325a0ba0c2f6a14fa6d98ea62758104
HcodNik <bruchikhin@gmail.com>
2023-11-23 14:06:38 +0300

    DZ # 5

--- manager.py
+++ manager.py
@@ -1,28 +1,21 @@
 import manager_module.menu_module
 from  manager_module.menu_module import *
-import os
 import manager_module.os_module
 from  manager_module.os_module import *
 
 
 
-def view_only_files():
-    cont_dir = os.listdir()
-    dir_files_only = []
-    for file in cont_dir:
-        if os.path.isfile(os.path.join(file)):
-            dir_files_only.append(file)
-    print(',\n'.join(dir_files_only))
 
 
-def view_only_folders():
-    cont_dir = os.listdir()
-    dir_files_only = []
-    for file in cont_dir:
-        if os.path.isfile(os.path.join(file)):
-            dir_files_only.append(file)
-    print(dir_files_only)
 
 
+
+
+
+
+
+# ГОТОВЫЕ КОНСТРУКЦИИ
+# print(f'Список файлов текущей директории:{os.getcwd()}\n{'\n'.join(view_file_only())}')
+# print(f'Список файлов текущей директории:{os.getcwd()}\n{'\n'.join(view_folder_only())}')
 
 
--- manager_module/os_module.py
+++ manager_module/os_module.py
@@ -2,6 +2,26 @@ import platform, os
 from manager_module.menu_module import main_menu_explorer
 
 
+
+
+def view_file_only(): # посмотреть ТОЛЬКО файлы текущей директории
+    cwd = os.listdir(os.getcwd()) # вывести список всех файлов и каталогов тек. директории
+    dir_file = []
+    for item in cwd:
+        if os.path.isfile(item) == True:
+            dir_file.append(item)
+    return dir_file
+    #return print(f'Список файлов текущей директории:{os.getcwd()}\n{'\n'.join(dir_file)}')
+
+def view_folder_only(): # посмотреть ТОЛЬКО папки текущей директории
+    cwd = os.listdir(os.getcwd()) # вывести список всех файлов и каталогов тек. директории
+    dir_fold = []
+    for item in cwd:
+        if os.path.isdir(item) == True:
+            dir_fold.append(item)
+    return dir_fold
+    #return print(f'Список папок текущей директории:{os.getcwd()}\n{'\n'.join(dir_fold)}')
+
 def sys_info(): # информация о системе
     os_info = platform.uname()
     print(f"Операционная система: {os_info.system} {os_info.release}")
@@ -10,7 +30,7 @@ def sys_info(): # информация о системе
     print(f"Архитектура: {','.join(platform.architecture())}")
     print(f"Процессор: {platform.processor()}")
 
-def del_file(): # удалить файл/папку
+def del_file(): # удалить файл/папку из тек. директории
     name_file = input('Введите имя файла для удаления: ')
     while os.path.exists(name_file) != True:
         print('Файл/папка с таким именем не найдены!')
@@ -23,7 +43,7 @@ def del_file(): # удалить файл/папку
         return os.remove(name_file)
     main_menu_explorer()
 
-def add_folder(): # создать каталог
+def add_folder(): # создать каталог в тек. директории
     name_fold = input('Введите имя каталога: ')
     while os.path.exists(name_fold) == True:
         print('Каталог с таким именем уже существует!')
@@ -36,8 +56,12 @@ def add_folder(): # создать каталог
 
 
     if __name__ == '__main__':
-        pass
+        print(os.getcwd())  # метод сообщает нам местоположение текущего рабочего каталога (CWD - Current working directory)
+        print(os.listdir(os.getcwd()))  # список всех файлов и каталогов в указанном каталоге, по умолчанию это текущий каталог
+        print(os.path.isfile(os.getcwd())) # проверяет, файл ли это
+        print(os.path.isdir(os.getcwd())) # проверяет, папка ли это
+        print(os.path.exists(os.getcwd())) # проверяет, существует ли указанный путь
 
 
 
-del_file()
+
