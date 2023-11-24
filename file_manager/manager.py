dc7e2fae607050a2d5dba3d1b5cdd0c39d98f8c1
HcodNik <bruchikhin@gmail.com>
2023-11-24 16:07:52 +0300

    DZ # 5

--- file_manager/manager.py
+++ file_manager/manager.py
@@ -1,3 +1,43 @@
-from author_mod import  author
+import os
+import os_mod
+from author_mod import author
+from menu_mod import main_menu_explorer
+from os_mod import *
+from del_mod import *
 
 
+
+# print(os.stat in os.supports_dir_fd)
+# print(os.pardir)
+# print(os.path.abspath(os.path.join(os.getcwd(), os.pardir))) # родительская директория
+
+# cwd = os.path.join(os.getcwd() + r'\111')
+
+
+# def sm_dir():
+#      global current_dir
+#      current_dir =  os.chdir(os.path.join(current_dir, '111'))
+
+
+# print(os.chdir(r'C:/Users/Admin/PycharmProjects/Console_File_Manager/file_manager/111'))
+#
+# print(current_dir)
+
+
+print(os_mod.__name__)
+
+# print(f'Старый путь: {os.getcwd()}')
+#
+# path = r'C:\Users\Admin\PycharmProjects\Console_File_Manager\file_manager\111'
+#
+# try:
+#     os.chdir(path)
+#     print("Current working directory: {0}".format(os.getcwd()))
+# except FileNotFoundError:
+#     print("Directory: {0} does not exist".format(path))
+# except NotADirectoryError:
+#     print("{0} is not a directory".format(path))
+# except PermissionError:
+#     print("You do not have permissions to change to {0}".format(path))
+#
+# print(f'Новый путь: {os.getcwd()}')
