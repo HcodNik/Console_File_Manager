430ada506964b1d2d98f637139b476c66f1b5660
HcodNik <bruchikhin@gmail.com>
2023-11-27 15:27:21 +0300

    DZ # 5

--- file_manager/sysinfo_mod.py
+++ file_manager/sysinfo_mod.py
@@ -1,12 +1,14 @@
 import platform
 
-def sys_info(): # информация о системе
+
+def sys_info():  # информация о системе
     os_info = platform.uname()
     print(f"Операционная система: {os_info.system} {os_info.release}")
     print(f"Сборка ОС: {os_info.version}")
     print(f"Имя устройства: {os_info.node}")
     print(f"Архитектура: {','.join(platform.architecture())}")
     print(f"Процессор: {platform.processor()}")
+
 
 if __name__ == '__main__':
     pass
