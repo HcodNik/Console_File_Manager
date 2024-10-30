63a310f8bb38682bbb938df0131f2f6de231b9dd
HcodNik <bruchikhin@gmail.com>
2024-10-30 15:53:27 +0300

    Добавлен новый функционал

--- manager.py
+++ manager.py
@@ -1,9 +1,8 @@
-import menu_mod 
+import menu_mod
 from menu_mod import *
 
 # ФАЙЛОВЫЙ МЕНЕДЖЕР - ЗАПУСК!!!
 
 
-if __name__ == '__main__':
-    main_menu_print()
-    main_menu_input()
+main_menu_print()
+main_menu_input()
